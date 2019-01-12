a08e3b41999c27f57457b2b098d07f9570a0b9f9
mrlaoyang <mrlaoyang@qq.com>
2019-01-12 19:47:13 +0800

    nothings

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ashy
+我的Python营销管理系统
+###2019.1.12
+alpha 0.0.1
+Sign in window,Main window,and some Menu
+###End
---
+++ main/main.py
@@ -0,0 +1,36 @@
+#coding:utf-8
+
+import Login
+import wx
+from Login import message       #导入Login包里的message变量
+
+Login       #调用登录界面
+
+class Frame(wx.Frame):
+    def __init__(self,parent,id):
+        wx.Frame.__init__(self,parent,id,title='爱尚花艺营销管理系统',pos=(135,25),size=(1024,700))   #窗口定义
+        icon = wx.Icon('..\images\ishy.ico', wx.BITMAP_TYPE_ICO)
+        self.SetIcon(icon)      #设置Frame标题的图标
+        panel = wx.Panel(self)      #创建画板容器
+        self.menubar = wx.MenuBar()     #新建菜单栏
+        self.menu1 = wx.Menu()      #新建菜单1
+        self.menubar.Append(self.menu1,'商品')        #将menu1加入菜单栏并命名
+        self.menuitem11 = wx.MenuItem(id=-1,text='新建')      #新建一个menuitem11命名为新建
+        self.menu1.Append(self.menuitem11)      #将menuitem11加入menu1
+        self.menu1.AppendSeparator()
+        self.menuitem12 =wx.MenuItem(id=-1,text='打开')
+        self.menu1.Append(self.menuitem12)
+        self.menu1.AppendSeparator()
+        self.menuitem13 =wx.MenuItem(id=-1,text='另存为')
+        self.menu1.Append(self.menuitem13)
+        self.menu1.AppendSeparator()
+        self.SetMenuBar(self.menubar)   #将菜单栏加入窗口
+        
+
+while True:
+    if message == '登录成功':
+        app = wx.App()          
+        frame = Frame(parent=None,id=-1)            
+        frame.Show()            
+        app.MainLoop()
+    break
