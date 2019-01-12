8188864b568553162b380ebfba7dbb16f788f2a5
mrlaoyang <mrlaoyang@qq.com>
2019-01-12 20:39:45 +0800

    NewThings

--- main/main.py
+++ main/main.py
@@ -14,16 +14,20 @@ class Frame(wx.Frame):
         panel = wx.Panel(self)      #创建画板容器
         self.menubar = wx.MenuBar()     #新建菜单栏
         self.menu1 = wx.Menu()      #新建菜单1
-        self.menubar.Append(self.menu1,'商品')        #将menu1加入菜单栏并命名
-        self.menuitem11 = wx.MenuItem(id=-1,text='新建')      #新建一个menuitem11命名为新建
+        self.menubar.Append(self.menu1,'商品管理')        #将menu1加入菜单栏并命名
+        self.menuitem11 = wx.MenuItem(id=-1,text='新建商品单')      #新建一个menuitem11并命名
         self.menu1.Append(self.menuitem11)      #将menuitem11加入menu1
         self.menu1.AppendSeparator()
-        self.menuitem12 =wx.MenuItem(id=-1,text='打开')
+        self.menuitem12 =wx.MenuItem(id=-1,text='打开商品单')
         self.menu1.Append(self.menuitem12)
-        self.menu1.AppendSeparator()
-        self.menuitem13 =wx.MenuItem(id=-1,text='另存为')
-        self.menu1.Append(self.menuitem13)
-        self.menu1.AppendSeparator()
+        
+        self.menu2 = wx.Menu()
+        self.menubar.Append(self.menu2,'进货管理')
+        self.menuitem21 = wx.MenuItem(id=-1,text='新建进货单')
+        self.menu2.Append(self.menuitem21)
+        self.menu2.AppendSeparator()
+        self.menuitem22 = wx.MenuItem(id=-1,text='打开进货单')
+        self.menu2.Append(self.menuitem22)
         self.SetMenuBar(self.menubar)   #将菜单栏加入窗口
         
 
