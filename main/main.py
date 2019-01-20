ff6ff42e4f0a157dc92c8a5fa5c200a30f2a9d69
mrlaoyang <mrlaoyang@qq.com>
2019-01-20 17:27:05 +0800

    a little Bug fixed

--- main/main.py
+++ main/main.py
@@ -9,7 +9,7 @@ Login       #调用登录界面
 class Frame(wx.Frame):
     def __init__(self,parent,id):
         wx.Frame.__init__(self,parent,id,title='爱尚花艺营销管理系统',pos=(135,25),size=(1024,700))   #窗口定义
-        icon = wx.Icon('..\images\ishy.ico', wx.BITMAP_TYPE_ICO)
+        icon = wx.Icon('..\\images\\ishy.ico', wx.BITMAP_TYPE_ICO)
         self.SetIcon(icon)      #设置Frame标题的图标
         panel = wx.Panel(self)      #创建画板容器
         self.menubar = wx.MenuBar()     #新建菜单栏
