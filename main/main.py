9f358bf489d1aa1eec32ab7012964d298be78710
mrlaoyang <mrlaoyang@qq.com>
2019-01-25 11:01:36 +0800

    Bind object

--- main/main.py
+++ main/main.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-
+import sys
+sys.path.append("../lib/")
+import MenuLib
 import Login
-
 import wx
-import sqlite3
-
 from Login import message       #导入Login包里的message变量
 
 Login       #调用登录界面
-
-connect = sqlite3.connect("../databases/database01.db")
-cursor = connect.cursor()
 
 class Frame(wx.Frame):
 	def __init__(self,parent,id):
@@ -22,24 +18,32 @@ class Frame(wx.Frame):
 		self.menubar = wx.MenuBar()     #新建菜单栏
 		self.menu1 = wx.Menu()      #新建菜单1
 		self.menubar.Append(self.menu1,'商品管理')        #将menu1加入菜单栏并命名
-		self.menuitem11 = wx.MenuItem(id=-1,text='新建')      #新建一个menuitem11并命名
-		self.menu1.Append(self.menuitem11)      #将menuitem11加入menu1
+
+		self.menuitem11 = self.menu1.Append(wx.ID_NEW,"新建")      #新加入一个menuitem11并命名
+		self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu11,self.menuitem11)
 		self.menu1.AppendSeparator()
-		self.menuitem12 =wx.MenuItem(id=-1,text='打开')
-		self.menu1.Append(self.menuitem12)
+
+		self.menuitem12 = self.menu1.Append(wx.ID_NEW,'打开')
+		self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu12,self.menuitem12)
 		self.menu1.AppendSeparator()
-		self.menuitem13 = wx.MenuItem(id=-1,text='编辑')      #新建一个menuitem13并命名
-		self.menu1.Append(self.menuitem13)      #将menuitem13加入menu1
+
+		self.menuitem13 = self.menu1.Append(wx.ID_NEW,'编辑')      #新加入一个menuitem13并命名
+		self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu13,self.menuitem13)
+
 		self.menu2 = wx.Menu()
 		self.menubar.Append(self.menu2,'进货管理')
-		self.menuitem21 = wx.MenuItem(id=-1,text='新建')
-		self.menu2.Append(self.menuitem21)
+
+		self.menuitem21 = self.menu2.Append(wx.ID_NEW,'新建')
+		self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu21,self.menuitem21)
 		self.menu2.AppendSeparator()
-		self.menuitem22 = wx.MenuItem(id=-1,text='打开')
-		self.menu2.Append(self.menuitem22)
+
+		self.menuitem22 = self.menu2.Append(wx.ID_NEW,'打开')
+		self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu22,self.menuitem22)
 		self.menu2.AppendSeparator()
-		self.menuitem23 = wx.MenuItem(id=-1,text='编辑')      #新建一个menuitem23并命名
-		self.menu2.Append(self.menuitem23)      #将menuitem23加入menu1
+
+		self.menuitem23 = self.menu2.Append(wx.ID_NEW,'编辑')      #新建一个menuitem23并命名
+		self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu23,self.menuitem23)      #绑定事件
+
 		self.SetMenuBar(self.menubar)   #将菜单栏加入窗口
 
 
