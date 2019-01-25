35c4d96a9c9fca2d840a0c7e2f92300caeb4111b
mrlaoyang <mrlaoyang@qq.com>
2019-01-25 12:20:48 +0800

    fix no module namede MenuLib bug

---
+++ lib/MenuLib.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+connect = sqlite3.connect("../databases/database01.db")
+cursor = connect.cursor()
+
+
+def OnClickMenu11(event):
+	global connect,cursor
+	print("Hello World")
+
+
+def OnClickMenu12(event):
+	global connect,cursor
+	print("Hello World")
+
+
+def OnClickMenu13(event):
+	global connect,cursor
+	print("Hello World")
+
+
+def OnClickMenu21(event):
+	global connect,cursor
+	print("Hello World")
+
+
+def OnClickMenu22(event):
+	global connect,cursor
+	print("Hello World")
+
+
+def OnClickMenu23(event):
+	global connect,cursor
+	print("Hello World")
+
