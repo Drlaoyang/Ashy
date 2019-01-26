0a59445c740d80c3bde7ac45e17119d28169a0dc
mrlaoyang <mrlaoyang@qq.com>
2019-01-26 18:49:18 +0800

    Fix cannot open the database01 bug

--- main/MenuLib.py
+++ main/MenuLib.py
@@ -11,27 +11,27 @@ def OnClickMenu11(event):       #menu11新建事件
     print("Hello World")
 
 
-def OnClickMenu12(event):
+def OnClickMenu12(event):       #menu12打开事件
     global connect,cursor
     print("Hello World")
 
 
-def OnClickMenu13(event):
+def OnClickMenu13(event):       #menu13编辑事件
     global connect,cursor
     print("Hello World")
 
 
-def OnClickMenu21(event):
+def OnClickMenu21(event):       #menu21新建事件
     global connect,cursor
     print("Hello World")
 
 
-def OnClickMenu22(event):
+def OnClickMenu22(event):       #menu22打开事件
     global connect,cursor
     print("Hello World")
 
 
-def OnClickMenu23(event):
+def OnClickMenu23(event):       #menu23编辑事件
     global connect,cursor
     print("Hello World")
 
