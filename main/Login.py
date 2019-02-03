fab418f0ee74c9c05a02d6e2a864b960006950fb
Drlaoyang <mrlaoyang@qq.com>
2019-02-03 10:20:05 +0800

    Get pep8.

--- main/Login.py
+++ main/Login.py
@@ -5,44 +5,49 @@ import wx
 
 message = ""
 
+
 class Frame(wx.Frame):
-    def __init__(self,parent,id):
-        wx.Frame.__init__(self,parent,id,title='爱尚花艺营销管理系统',pos=(450,200))
+    def __init__(self, parent, id):
+        wx.Frame.__init__(self, parent, id, title='爱尚花艺营销管理系统', pos=(450, 200))
         icon = wx.Icon('./images/ishy.ico', wx.BITMAP_TYPE_ICO)
-        self.SetIcon(icon) #设置Frame标题的图标
+        self.SetIcon(icon)  # 设置Frame标题的图标
         panel = wx.Panel(self)
-        
-        self.title = wx.StaticText(panel,label='请输入用户名和密码')
-        self.font = wx.Font(16,wx.DEFAULT,wx.FONTSTYLE_NORMAL,wx.NORMAL)
-        self.label_user = wx.StaticText(panel,label='用户名:')
-        self.text_user = wx.TextCtrl(panel,size=(235,25),style=wx.TE_LEFT)
-        self.label_pwd = wx.StaticText(panel,label='密  码:')
-        self.text_pwd = wx.TextCtrl(panel,size=(235,25),style=wx.TE_PASSWORD)
-        self.bt_confrim = wx.Button(panel,label='OK')
-        self.bt_confrim.Bind(wx.EVT_BUTTON,self.OnclickSubmit)
-        self.bt_cancel = wx.Button(panel,label='cancel')
-        self.bt_cancel.Bind(wx.EVT_BUTTON,self.OnclickCancel)
-        
+
+        self.title = wx.StaticText(panel, label='请输入用户名和密码')
+        self.font = wx.Font(16, wx.DEFAULT, wx.FONTSTYLE_NORMAL, wx.NORMAL)
+        self.label_user = wx.StaticText(panel, label='用户名:')
+        self.text_user = wx.TextCtrl(panel, size=(235, 25), style=wx.TE_LEFT)
+        self.label_pwd = wx.StaticText(panel, label='密  码:')
+        self.text_pwd = wx.TextCtrl(
+            panel, size=(235, 25), style=wx.TE_PASSWORD)
+        self.bt_confrim = wx.Button(panel, label='OK')
+        self.bt_confrim.Bind(wx.EVT_BUTTON, self.OnclickSubmit)
+        self.bt_cancel = wx.Button(panel, label='cancel')
+        self.bt_cancel.Bind(wx.EVT_BUTTON, self.OnclickCancel)
+
         h_user = wx.BoxSizer(wx.HORIZONTAL)
-        h_user.Add(self.label_user,proportion=0,flag=wx.ALL,border=5)
-        h_user.Add(self.text_user,proportion=1,flag=wx.ALL,border=5)
+        h_user.Add(self.label_user, proportion=0, flag=wx.ALL, border=5)
+        h_user.Add(self.text_user, proportion=1, flag=wx.ALL, border=5)
         h_pwd = wx.BoxSizer(wx.HORIZONTAL)
-        h_pwd.Add(self.label_pwd,proportion=0,flag=wx.ALL,border=5)
-        h_pwd.Add(self.text_pwd,proportion=1,flag=wx.ALL,border=5)
+        h_pwd.Add(self.label_pwd, proportion=0, flag=wx.ALL, border=5)
+        h_pwd.Add(self.text_pwd, proportion=1, flag=wx.ALL, border=5)
         h_button = wx.BoxSizer(wx.HORIZONTAL)
-        h_button.Add(self.bt_confrim,proportion=0,flag=wx.ALL,border=5)
-        h_button.Add(self.bt_cancel,proportion=0,flag=wx.ALL,border=5)
-        
+        h_button.Add(self.bt_confrim, proportion=0, flag=wx.ALL, border=5)
+        h_button.Add(self.bt_cancel, proportion=0, flag=wx.ALL, border=5)
+
         v_all = wx.BoxSizer(wx.VERTICAL)
-        v_all.Add(self.title,proportion=0,flag=wx.BOTTOM|wx.TOP|wx.ALIGN_CENTER,
-            border=15)
-        v_all.Add(h_user,proportion=0,flag=wx.EXPAND|wx.LEFT|wx.RIGHT,border=45)
-        v_all.Add(h_pwd,proportion=0,flag=wx.EXPAND|wx.LEFT|wx.RIGHT,border=45)
-        v_all.Add(h_button,proportion=0,flag=wx.ALIGN_CENTER|wx.TOP,border=15)
-        
+        v_all.Add(self.title, proportion=0, flag=wx.BOTTOM |
+                  wx.TOP | wx.ALIGN_CENTER, border=15)
+        v_all.Add(h_user, proportion=0, flag=wx.EXPAND |
+                  wx.LEFT | wx.RIGHT, border=45)
+        v_all.Add(h_pwd, proportion=0, flag=wx.EXPAND |
+                  wx.LEFT | wx.RIGHT, border=45)
+        v_all.Add(h_button, proportion=0,
+                  flag=wx.ALIGN_CENTER | wx.TOP, border=15)
+
         panel.SetSizer(v_all)
-    
-    def OnclickSubmit(self,event):
+
+    def OnclickSubmit(self, event):
         global message
         username = self.text_user.GetValue()
         password = self.text_pwd.GetValue()
@@ -58,12 +63,17 @@ class Frame(wx.Frame):
             wx.MessageBox('(确定后点右上角X)')
         else:
             wx.MessageBox(message)
-    def OnclickCancel(self,event):
+
+    def OnclickCancel(self, event):
         self.text_user.SetValue('')
         self.text_pwd.SetValue('')
 
+
 if True:
-    app = wx.App()          
-    frame = Frame(parent=None,id=-1)            
-    frame.Show()            
-    app.MainLoop()          
+    app = wx.App()
+    frame = Frame(parent=None, id=-1)
+    frame.Show()
+    app.MainLoop()
+ame = Frame(parent=None, id=-1)
+frame.Show()
+app.MainLoop()
--- main/MenuLib.py
+++ main/MenuLib.py
@@ -6,32 +6,31 @@ connect = sqlite3.connect("./databases/database01.db")
 cursor = connect.cursor()
 
 
-def OnClickMenu11(event):       #menu11新建事件
-    global connect,cursor
+def OnClickMenu11(event):  # menu11新建事件
+    global connect, cursor
     print("Hello World")
 
 
-def OnClickMenu12(event):       #menu12打开事件
-    global connect,cursor
+def OnClickMenu12(event):  # menu12打开事件
+    global connect, cursor
     print("Hello World")
 
 
-def OnClickMenu13(event):       #menu13编辑事件
-    global connect,cursor
+def OnClickMenu13(event):  # menu13编辑事件
+    global connect, cursor
     print("Hello World")
 
 
-def OnClickMenu21(event):       #menu21新建事件
-    global connect,cursor
+def OnClickMenu21(event):  # menu21新建事件
+    global connect, cursor
     print("Hello World")
 
 
-def OnClickMenu22(event):       #menu22打开事件
-    global connect,cursor
+def OnClickMenu22(event):  # menu22打开事件
+    global connect, cursor
     print("Hello World")
 
 
-def OnClickMenu23(event):       #menu23编辑事件
-    global connect,cursor
+def OnClickMenu23(event):  # menu23编辑事件
+    global connect, cursor
     print("Hello World")
-
