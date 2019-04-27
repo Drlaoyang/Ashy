346891e58b6f653167462cb37e38c74c2170884f
Drlaoyang <mrlaoyang@qq.com>
2019-04-27 15:04:21 +0800

    Redo.

--- README.md
+++ README.md
@@ -2,13 +2,7 @@
 
 # Ashy
 My New Python File    
-	
-2019.4.27	
-Bind some event.	
-Big Redo.	
 
-2019.4.27	    
-alpha 0.0.1    	
-Sign in window,Main window,and some Menu.    
-fix the IDE bug,like sublime cannot use the "main.py" and "Login".    
-End     
+2019.4.28    
+Redo.  
+准备重新开始。  
--- main/main.py
+++ main/main.py
@@ -19,29 +19,29 @@ class Frame(wx.Frame):
         self.menu1 = wx.Menu()      #新建菜单1
         self.menubar.Append(self.menu1,'商品管理')        #将menu1加入菜单栏并命名
 
-        self.menuitem11 = self.menu1.Append(wx.ID_NEW,"新建")      #新加入一个menuitem11并命名
+        self.menuitem11 = self.menu1.Append(11,"新建")      #新加入一个menuitem11并命名
         self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu11,self.menuitem11)
         self.menu1.AppendSeparator()
 
-        self.menuitem12 = self.menu1.Append(wx.ID_NEW,'打开')
+        self.menuitem12 = self.menu1.Append(12,'打开')
         self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu12,self.menuitem12)
         self.menu1.AppendSeparator()
 
-        self.menuitem13 = self.menu1.Append(wx.ID_NEW,'编辑')      #新加入一个menuitem13并命名
+        self.menuitem13 = self.menu1.Append(13,'编辑')      #新加入一个menuitem13并命名
         self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu13,self.menuitem13)
 
         self.menu2 = wx.Menu()
         self.menubar.Append(self.menu2,'进货管理')
 
-        self.menuitem21 = self.menu2.Append(wx.ID_NEW,'新建')
+        self.menuitem21 = self.menu2.Append(21,'新建')
         self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu21,self.menuitem21)
         self.menu2.AppendSeparator()
 
-        self.menuitem22 = self.menu2.Append(wx.ID_NEW,'打开')
+        self.menuitem22 = self.menu2.Append(22,'打开')
         self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu22,self.menuitem22)
         self.menu2.AppendSeparator()
 
-        self.menuitem23 = self.menu2.Append(wx.ID_NEW,'编辑')      #新建一个menuitem23并命名
+        self.menuitem23 = self.menu2.Append(23,'编辑')      #新建一个menuitem23并命名
         self.Bind(wx.EVT_MENU,MenuLib.OnClickMenu23,self.menuitem23)      #绑定事件
 
         self.SetMenuBar(self.menubar)   #将菜单栏加入窗口
