0dcc80d2d8bbf32abdaa693d09e354b8e9010564
Drlaoyang <mrlaoyang@qq.com>
2019-05-11 08:45:47 +0800

    优化多余代码

--- main/MenuLib.py
+++ main/MenuLib.py
@@ -2,35 +2,24 @@
 # -*- coding:utf-8 -*-
 import sqlite3
 
-connect = sqlite3.connect("../src/databases/database.db")
-cursor = connect.cursor()
-
+global cursor, connect #创建全局数据库对象
+connect = sqlite3.connect("../src/databases/database.db") #实例化对象
+cursor = connect.cursor() #实例化对象
 
 def OnClickMenu11(event):  # menu11新建事件
-    global connect, cursor
     print("Hello World")
-
 
 def OnClickMenu12(event):  # menu12打开事件
-    global connect, cursor
     print("Hello World")
-
 
 def OnClickMenu13(event):  # menu13编辑事件
-    global connect, cursor
     print("Hello World")
-
 
 def OnClickMenu21(event):  # menu21新建事件
-    global connect, cursor
     print("Hello World")
-
 
 def OnClickMenu22(event):  # menu22打开事件
-    global connect, cursor
     print("Hello World")
 
-
 def OnClickMenu23(event):  # menu23编辑事件
-    global connect, cursor
     print("Hello World")
